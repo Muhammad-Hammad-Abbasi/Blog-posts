176ded83ccae88767abd9396534246300b732f3c
Muhammad-Hammad-Abbasi <hammadabbasi102030@gmail.com>
2024-12-16 20:25:18 -0800

    update.

--- src/app/blogs/blogcard/page.tsx
+++ src/app/blogs/blogcard/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-export interface Card_Type{
+ interface Card_Type{
   heading: string;
   text: string;
   blogid: string;
